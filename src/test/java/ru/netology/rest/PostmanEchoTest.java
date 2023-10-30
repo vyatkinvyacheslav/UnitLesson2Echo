6925668a3331259222111ea9b4eb4159d3a59989
Vyacheslav Vyatkin <Vyatkin.vyacheslav.a@yandex.ru>
2023-10-30 23:40:53 +0500

    error commit

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSendData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(300)
+                .body("data", equalTo("Hello"));
+    }
+
+}
